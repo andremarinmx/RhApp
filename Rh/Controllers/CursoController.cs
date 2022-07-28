c8a2b73b0e912e2a52ead5a5e08954ee98555e15
Andre Marin <jmarin29@jci.com>
2022-07-28 16:20:06 -0600

    Crear, eliminar y editar cursos

--- Rh/Controllers/CursoController.cs
+++ Rh/Controllers/CursoController.cs
@@ -10,22 +10,90 @@ namespace Rh.Controllers
     public class CursoController : Controller
     {
         public List<Curso> Cursos { get; set; }
+        public List<Instructor> Instructores { get; set; }
 
         [HttpGet]
         public ActionResult CrearCurso()
         {
-            return View();
+            return View(Instructores);
         }
 
         [HttpPost]
-        public ActionResult CrearInstructores(string nombre, int duracion, int calificacion, string tipo, string clasificacion, string areaTematica, string areaAplicacion, string objetivo, string modalidad, string comentarios, int instructor)
+        public ActionResult CrearCurso(string nombre, int duracion, int calificacion, string tipo, string clasificacion, string areaTematica, string areaAplicacion, string objetivo, string modalidad, string comentarios, int instructor)
         {
             using (AndreTestContext db = new AndreTestContext())
             {
                 Curso cur = new Curso();
+                Instructor inst = new Instructor() { ID_INSTRUCTOR = instructor};
+                db.Instructors.Attach(inst);
+                cur.NOMBRE = nombre;
+                cur.DURACION = duracion;
+                cur.CALIFICACION_MIN = calificacion;
+                cur.TIPO_CURSO = tipo;
+                cur.CLASIFICACION = clasificacion;
+                cur.AREA_TEMATICA = areaTematica;
+                cur.AREA_APLICACION = areaAplicacion;
+                cur.OBJETIVO = objetivo;
+                cur.MODALIDAD = modalidad;
+                cur.COMENTARIOS = comentarios;
+                cur.Instructor = inst;
                 db.Cursoes.Add(cur);
                 db.SaveChanges();
                 Cursos = db.Cursoes.OrderBy(x => x.ID_CURSO).ToList();
+                return RedirectToAction("VerCursos");
+            }
+        }
+
+        [HttpGet]
+        public ActionResult VerCursos()
+        {
+            using (AndreTestContext db = new AndreTestContext())
+            {
+                Cursos = db.Cursoes.OrderBy(x => x.ID_CURSO).ToList();
+                return View(Cursos);
+            }
+        }
+
+        [HttpGet]
+        public ActionResult EliminarCurso(int id)
+        {
+            using (AndreTestContext db = new AndreTestContext())
+            {
+                Curso cur = db.Cursoes.Find(id);
+                db.Cursoes.Remove(cur);
+                db.SaveChanges();
+                return RedirectToAction("VerCursos");
+            }
+        }
+
+        [HttpGet]
+        public ActionResult EditarCurso(int id)
+        {
+            using (AndreTestContext db = new AndreTestContext())
+            {
+                Curso cur = db.Cursoes.Find(id);
+                return View(cur);
+            }
+        }
+
+        [HttpPost]
+        public ActionResult EditarCurso(Curso x, string tipo, string clasificacion, string areaTematica, string areaAplicacion, string objetivo, string modalidad)
+        {
+            using (AndreTestContext db = new AndreTestContext())
+            {
+                Curso cur = db.Cursoes.Find(x.ID_CURSO);
+                cur.NOMBRE = x.NOMBRE;
+                cur.DURACION = x.DURACION;
+                cur.CALIFICACION_MIN = x.CALIFICACION_MIN;
+                cur.TIPO_CURSO = tipo;
+                cur.CLASIFICACION = clasificacion;
+                cur.AREA_TEMATICA = areaTematica;
+                cur.AREA_APLICACION = areaAplicacion;
+                cur.OBJETIVO = objetivo;
+                cur.MODALIDAD = modalidad;
+                cur.COMENTARIOS = x.COMENTARIOS;
+                cur.Instructor = x.Instructor;
+                db.SaveChanges();
                 return RedirectToAction("VerCursos");
             }
         }
