7729429e44cf80458f150cd23cb8b330bd7e0763
Andre Marin <jmarin29@jci.com>
2022-07-29 17:02:33 -0600

    Vista parcial de empleados

---
+++ Rh/Controllers/EmpleadoController.cs
@@ -0,0 +1,43 @@
+﻿using Rh.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Rh.Controllers
+{
+    public class EmpleadoController : Controller
+    {
+        public List<Empleado> Empleado { get; set; }
+
+        [HttpGet]
+        public ActionResult CrearEmpleado()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult CrearEmpleado(Empleado y)
+        {
+            using (AndreTestContext db = new AndreTestContext())
+            {
+                Empleado emp = new Empleado();
+                emp.NOMBRE = y.NOMBRE;
+                db.SaveChanges();
+                Empleado = db.Empleadoes.OrderBy(x => x.ID_EMPLEADO).ToList();
+                return RedirectToAction("VerEmpleados");
+            }
+        }
+
+        [HttpGet]
+        public ActionResult VerEmpleados()
+        {
+            using (AndreTestContext db = new AndreTestContext())
+            {
+                Empleado = db.Empleadoes.OrderBy(x => x.ID_EMPLEADO).ToList();
+                return View(Empleado);
+            }
+        }
+    }
+}
